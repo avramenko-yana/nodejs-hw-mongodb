cd62d39fa8acc025501f44d68aabed839bc27d12
Baxiboo <4avramenko.yana@gmail.com>
2025-09-01 19:31:23 +0300

    first

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export const initMongoConnection = async () => {
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
+  const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+
+  try {
+    await mongoose.connect(connectionString);
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.error('Failed to connect to Mongo:', error);
+    process.exit(1);
+  }
+};
---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import contactsRouter from './routers/contacts.js';
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }));
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.use('/contacts', contactsRouter);
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
