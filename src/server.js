f5563eb2eb98e9836c2d4bf4b2ef17fffc33e246
Baxiboo <4avramenko.yana@gmail.com>
2025-09-10 17:32:13 +0300

    hw7-1

--- src/server.js
+++ src/server.js
@@ -2,23 +2,29 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'node:fs/promises';
+
 import contactsRouter from './routers/contacts.js';
-import authRouter from './routers/auth.js';  
+import authRouter from './routers/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+ 
+const swaggerDocument = JSON.parse(await fs.readFile('docs/swagger.json', 'utf8'));
 
 export const setupServer = () => {
   const app = express();
+ 
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use(pino({ transport: { target: 'pino-pretty' } }));
   app.use(cors());
-  app.use(cookieParser());  
+  app.use(cookieParser());
   app.use(express.json());
 
-  // Роутери
-  app.use('/auth', authRouter); 
+  app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
- 
+
   app.use(notFoundHandler);
   app.use(errorHandler);
 
